ea65e6395cda4c8a4bd90091ba480d933721a615
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-03 21:53:43 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,1 +1,15 @@
-
+window.addEventListener('load', function() {
+  var canvas = document.getElementById('website');
+  var ctx = canvas.getContext && canvas.getContext('2d');
+  if(ctx) {
+    website(canvas, ctx);
+  }
+});
+var website = function(cvs, ctx) {
+  var w = screen.width;
+  var h = screen.height
+  cvs.width = w;
+  cvs.height = h;
+  cvs.fillStyle = "#000";
+  cvs.fillRect(0, 0, w, h);
+}
