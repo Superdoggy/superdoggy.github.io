c52c5b9b7dda67c30a2e4a643f6081d2c89d92e9
Superdoggy <superdoggy.pr0gr4mm3r@gmail.com>
2015-08-03 22:06:26 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,15 +1,24 @@
+var cvs;
+var ctx;
+var w;
+var h;
 window.addEventListener('load', function() {
   var canvas = document.getElementById('website');
-  var ctx = canvas.getContext && canvas.getContext('2d');
-  if(ctx) {
-    website(canvas, ctx);
+  context = canvas.getContext && canvas.getContext('2d');
+  if(context) {
+    cvs = canvas;
+    ctx = context;
+    website();
   }
 });
-var website = function(cvs, ctx) {
-  var w = window.innerWidth;
-  var h = window.innerHeight
+var background = function() {
+  w = window.innerWidth;
+  h = window.innerHeight;
   cvs.width = w;
   cvs.height = h;
   ctx.fillStyle = "#000";
   ctx.fillRect(0, 0, w, h);
+}
+var website = function() {
+  setTimeout(background(), 16);
 }
